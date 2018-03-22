6d9b7f3fffc951d1bda2c82c38559fe867fd214d
project <project@email.com>
2018-03-22 23:54:07 +0700

    add reacstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,35 @@ import { Link } from "react-router-dom";
 import { observer,inject } from "mobx-react";
 import { withRouter } from 'react-router'
 
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Button
+} from 'reactstrap';
+
 @inject("store")
 @withRouter
 @observer
 export default class extends Component {
     nav = this.props.routes.map((route,index) => {
-        return route.onMenu?<li className="nav-item" key={index}>
+        return route.onMenu?<NavItem key={index}>
                 <Link className="nav-link" to={route.path}>
                     {route.name}
                 </Link>
-                </li>:""
-      
-        
+                </NavItem>:""        
     })  
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen:false
+        }    
+    }
     
     Login = () => {
         //console.log(this.props.store)
@@ -29,21 +45,29 @@ export default class extends Component {
             history.push('/login')
     }
 
+    toggle = () => {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+
     render() {
         let { store : {authStore}} = this.props
-        return <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="navbar-brand">
+        return   <Navbar color="primary" dark expand="md">
+                 <NavbarBrand href="/">
                     Project
-                </div>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav  navbar>
                     {this.nav}
-                </ul>
-                <button className="btn btn-primary" onClick={this.Login}>
+                </Nav>
+                <Button color="primary"  onClick={this.Login}>
                     {!authStore.isLogin?"Login":"Logout"}
-                    </button>
-                </div>
-                </nav>        
+                </Button>
+                
+                </Collapse>
+                </Navbar >        
     }
 }
 
